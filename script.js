00b159f4dc9db97209c1748506aadcfb3419e3ef
Tim Kaiser <kaisertimothy@johndeere.com>
2020-09-11 08:56:00 -0500

    completed basic game board

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('up and running');
+
+//Event listener for start round button
+
+//Event listener for new game button
+
+//Game logic
+//Array to store sequence
+//Sequence will be generated randomly
+
+//Game Play
+//after round is started, sequence starts and blinks the first glass
+//player must click glass that just blinked
+//if player clicks correct glass another glass is added to the array 
+//new array is blinked
+
+//Win logic
+//If array length is 5 and player successfully completes round player wins
+//Logic to add score if round is complete
+
+//If player loses throw message or blink glasses red. 
